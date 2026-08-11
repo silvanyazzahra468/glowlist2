cb5a9163b82abec8380e930ee2d13829a512cc7c
silvanyazzahra468 <haechanah45@gmail.com>
2026-08-11 10:37:23 +0700

    Update server.js

--- glowlist-backend/server.js
+++ glowlist-backend/server.js
@@ -1,8 +1,43 @@
 const express = require('express');
 const app = express();
+const mysql = require('mysql2');
+
+const db = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'glowlist_db'
+});
+
+db.connect(err => {
+    if (err) {
+        console.error('Gagal konek ke database:', err);
+    } else {
+        console.log('Berhasil konek e database GlowList');
+    }
+    });
+
+    app.get('/produk', (req,res) => {
+        const sql = 'SELECT *FROM produk';
+        db.query(sql, (err, result) => {
+            if (err) return res.status(500).json({ error: err });
+            res.json(result);
+        });
+    })
+
+    app.get('/kategori', (req,res) => {
+        const sql = 'SELECT *FROM kategori';
+        db.query(sql, (err, result) => {
+            if (err) return res.status(500).json({ error: err });
+            res.json(result);
+        });
+    })
+
+
 const PORT = 3001;
 
 app.use(express.json());
+
 
 app.get('/', (req, res) => {
     res.send('SELAMAT DATANG DI GLOWLIST ');
